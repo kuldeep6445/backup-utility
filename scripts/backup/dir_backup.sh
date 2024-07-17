4bca9a36a0312aa99f7442331569fe2251467c4f
kuldeep6445 <kuldeepbhogal111@gmail.com>
2024-07-17 16:08:17 +0530

    complete

--- scripts/backup/dir_backup.sh
+++ scripts/backup/dir_backup.sh
@@ -4,8 +4,8 @@ owner=$(echo "$current_dir" | cut -d '/' -f3)
 
 curr_path="/home/$owner"
 
-#sudo rsync -a --info=progress2 "$curr_path/Desktop" data/dir/
-#sudo rsync -a --info=progress2 "$curr_path/Documents" data/dir/
+sudo rsync -a --info=progress2 "$curr_path/Desktop" data/dir/
+sudo rsync -a --info=progress2 "$curr_path/Documents" data/dir/
 sudo rsync -a --info=progress2 "$curr_path/common" data/dir/
-#sudo rsync -a --info=progress2 "$curr_path/Downloads" data/dir/
+sudo rsync -a --info=progress2 "$curr_path/Downloads" data/dir/
 
--- scripts/restore/dir_restore.sh
+++ scripts/restore/dir_restore.sh
@@ -4,7 +4,7 @@ owner=$(echo "$current_dir" | cut -d '/' -f3)
 
 curr_path="/home/$owner"
 
-#rsync -a --info=progress2 data/dir/Desktop ~/
-#rsync -a --info=progress2 data/dir/Documents ~/
+rsync -a --info=progress2 data/dir/Desktop ~/
+rsync -a --info=progress2 data/dir/Documents ~/
 rsync -a --info=progress2 data/dir/common ~/
-#rsync -a --info=progress2 data/dir/Downloads ~/
+rsync -a --info=progress2 data/dir/Downloads ~/
